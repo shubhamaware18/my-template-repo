22c0b606724d71a8e0c61f333901a3e4ad12bd47
Shubham Aware <sandyaware07@gmail.com>
2023-12-31 18:53:29 +0530

    folder stucture of project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Thisn is the demo project developed o n the concept of MLOps',
+    author='Shubham Aware',
+    license='',
+)
